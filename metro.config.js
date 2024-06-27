e130245e00aa1255ac8921cf6da742b3698f5cde
matheusprovin <matheus.ehlert@villelabrasillabs.com>
2024-06-27 14:01:19 -0300

    build expo do metroconfig

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+/** @format */
+
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.sourceExts.push("cjs");
+
+module.exports = defaultConfig;
